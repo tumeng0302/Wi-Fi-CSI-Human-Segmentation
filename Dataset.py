8dbf4220727c3834455b11c1bc1d433e7c5fe37a
tumeng0302 <tumeng0302@gmail.com>
2024-06-17 06:56:16 +0000

    remove denoise

--- Dataset.py
+++ Dataset.py
@@ -8,7 +8,7 @@ import torch
 import json
 
 def get_data_list(split: str, data_root: str):
-    valid_type = ['train', 'train_1', 'train_2', 'train_3', 'val', 'test', 'occu']
+    valid_type = ['train', 'train_1', 'train_2', 'train_3', 'val', 'test', 'occu', 'random']
     if split not in valid_type:
         raise ValueError(f'Invalid split type: {split}, should be one of {valid_type}')
     
@@ -83,7 +83,7 @@ class CSI_Dataset(Dataset):
         """
         Args:
             data_root: str, path to the dataset
-            split: str, should be in: ['train', 'train_1', 'train_2', 'train_3', 'val', 'test', 'occu'], default 'train'
+            split: str, should be in: ['train', 'train_1', 'train_2', 'train_3', 'val', 'test', 'occu', 'random'], default 'train'
             crop_size: tuple, size of the image. default (192, 256)
             interpolation: 
                 float, the probability of use interpolation to augment the data
@@ -98,8 +98,6 @@ class CSI_Dataset(Dataset):
         self.data_root = data_root
         self.data_list = get_data_list(split, data_root)
         self.split = split
-        self.b_cha, self.a_cha = signal.butter(5, 100, 'lowpass', fs=2025)
-        self.b_tem, self.a_tem = signal.butter(5, 90, 'lowpass', fs=500)
         self.data_struct = {}
         for data in self.data_list:
             env = data[0].split('/')[0]
@@ -122,16 +120,6 @@ class CSI_Dataset(Dataset):
     def normalize(self, x: torch.Tensor, mean: float = 0, std: float = 0.5):
         return ((x - x.mean()) / x.std()) * std + mean
 
-    def denoise(self, x: np.ndarray, type: str):
-        x_filt = signal.filtfilt(self.b_cha, self.a_cha, x, axis=2)
-        x_filt = signal.filtfilt(self.b_tem, self.a_tem, x_filt, axis=0)
-        if type == 'amp':
-            x_filt[:, :, 1001:1024] = 0
-            x_filt[:, :, 1997:] = 0
-        elif type == 'pha':
-            x_filt[:, :, 1997:] = 0
-        return x_filt
-
     def get_data(self, mask_path, data_path, npy_path)->tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         mask = Image.open(f'{self.data_root}/{mask_path}_mask.png').convert('L')
         mask = self.transform(mask).float()
@@ -141,7 +129,7 @@ class CSI_Dataset(Dataset):
             amplist.append(data['mag'].astype(np.float32)/self.amp_offset)
             phalist.append(data['pha'].astype(np.float32)/self.pha_offset)
         amp, pha = np.concatenate(amplist, axis=0), np.concatenate(phalist, axis=0)
-        amp, pha = torch.from_numpy(self.denoise(amp, 'amp').copy()).float(), torch.from_numpy(self.denoise(pha, 'pha').copy()).float()
+        amp, pha = torch.from_numpy(amp), torch.from_numpy(pha)
         amp, pha = self.normalize(amp), self.normalize(pha)
         if self.split == 'train' and np.random.random() < self.interpolation:
             amp, pha = interpolation(amp, pha)
